2523ad085f140ae1467859f5f26090499431ca25
delshay <damian.antoniak@hotmail.com>
2020-03-22 11:24:32 +0100

    Delete unnecessary comments

--- src/dev/antoniak/randomnumbergenerator/RandomNumberGenerator.java
+++ src/dev/antoniak/randomnumbergenerator/RandomNumberGenerator.java
@@ -10,13 +10,6 @@ public class RandomNumberGenerator {
         this.random = new Random();
     }
 
-    /**
-     * This is my attempt to generate correct random double in given range.
-     *
-     * @param min
-     * @param max
-     * @returnm
-     */
     public double fromRange(int min, int max) {
         return min + (max - min) * this.random.nextDouble();
     }
